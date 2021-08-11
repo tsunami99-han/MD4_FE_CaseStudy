d6f9c699f68b17f614bade5b6ed2c5b96097cd8e
luctr <dinh.luc.3192@gmail.com>
2021-08-11 09:49:11 +0700

    add js folder

--- Case-study-md4-wt/js/user.js
+++ Case-study-md4-wt/js/user.js
@@ -6,38 +6,66 @@ function addNewUser() {
     let gender = $('#gender').val();
     let address = $('#address').val();
     let phone = $('#phoneNumber').val();
-    if (name,password,fullName,address != ""){
-        document.getElementById('sigup').disabled = false;
-    }
-    if (name == ""){
-        document.getElementById('messageName').innerHTML= `<p>acv</p>`
-    }
     let newUser = {
-        name: name,
+        username: name,
         password: password,
         fullName: fullName,
-        date: date,
-        address :address,
+        dateOfBirth: date,
+        address: address,
         gender: gender,
-        roles: {
-            id : 2
-        },
-        phone:phone
+        phoneNumber: phone
     };
+    console.log(newUser)
     $.ajax({
-        //chuyển dữ liệu vè json
-        headers: { 'Accept': 'application/json',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: 'POST',
+        data: JSON.stringify(newUser),
+        url: 'http://localhost:8080/users/register',
+
+        success: function (data) {
+            alert("ok")
+        },
+        error: function () {
+            alert("error")
+        },
+
+    });
+
+    // event.preventDefault();
+}
+
+function login(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let user = {
+        username: username,
+        password: password
+    }
+    console.log(user)
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         type: "POST",
-        data: JSON.stringify(newUser),
-        url: "/users",
-        // success: successHandler
+        data: JSON.stringify(user),
+        url: 'http://localhost:8080/users/login',
+            success: function (data){
+                console.log(data)
+                localStorage.setItem("token", data.accessToken)
+                localStorage.setItem("id", data.id)
+                window.location.href='test.html'
+            },
+         error:function () {
+             alert("TêN tài khoản hoặc mật khẩu không đúng")
+         }
+        })
+    }
 
-    });
-    // event.preventDefault();
-}
-function check(){
+function check() {
     let account = document.getElementById('account').value;
     let password = $('#password').val();
     let fullName = $('#fullName').val();
@@ -45,18 +73,31 @@ function check(){
     let gender = $('#gender').val();
     let address = $('#address').val();
     let phone = $('#phoneNumber').val();
-    if (account == ""){
-        document.getElementById('messageName').innerHTML = `<p> Name Account Không Được Để Trống </p>`}
-    if (password == ""){
-        document.getElementById('messagePassword').innerHTML = `<p> Password Không Được Để Trống </p>`}
-    if (fullName == ""){
-        document.getElementById('messageFullName').innerHTML = `<p> Full Name Không Được Để Trống </p>`}
-    if (date == ""){
-        document.getElementById('messageDate').innerHTML = `<p> Date Không Được Để Trống </p>`}
-    if (gender == ""){
-        document.getElementById('messageGenders').innerHTML = `<p> Gender Không Được Để Trống </p>`}
-    if (address == ""){
-        document.getElementById('messageAddress').innerHTML = `<p> Address Không Được Để Trống </p>`}
-    if (phone == ""){
-        document.getElementById('messagePhoneNumber').innerHTML = `<p> Phone Number Không Được Để Trống </p>`}
-    };
+    console.log(gender)
+    if (account === "") {
+        document.getElementById('messageName').innerHTML = `<p> Name Account Không Được Để Trống </p>`
+    } else if (password === "") {
+        document.getElementById('messagePassword').innerHTML = `<p> Password Không Được Để Trống </p>`
+    }
+    if (fullName === "") {
+        document.getElementById('messageFullName').innerHTML = `<p> Full Name Không Được Để Trống </p>`
+    }
+    if (date === "") {
+
+        document.getElementById('messageDate').innerHTML = `<p> Date Không Được Để Trống </p>`
+    }
+    if (gender === "") {
+        document.getElementById('messageGenders').innerHTML = `<p> Gender Không Được Để Trống </p>`
+    }
+    if (address === "") {
+        document.getElementById('messageAddress').innerHTML = `<p> Address Không Được Để Trống </p>`
+    }
+    if (phone === "") {
+        document.getElementById('messagePhoneNumber').innerHTML = `<p> Phone Number Không Được Để Trống </p>`
+    }
+    if (account !== "" && password !== "" && fullName !== "" && date !== "" && gender !== "" && address !== "" && phone !== "") {
+        console.log("vao");
+
+        $('#sigup').prop('disabled', false);
+    }
+};
